2643a79e5aab1362f34bac6fc7fe542baf5047bb
Shantanu Pavgi <pavgi@uab.edu>
2012-04-16 13:25:38 -0500

    * Updated 'installation' class with changes in 'degems' and 'depackages' classes - new variable names, require/dependency calls etc. * Updated instalation script to remove all files in installation directory before starting the installation process

--- manifests/install/installation.pp
+++ manifests/install/installation.pp
@@ -1,59 +1,76 @@
 # = Class: uabopennebula::install::installation
 #
-# Creates an installation script based on given parameters and then executes the script.
-# Before execution it calls uabopennebula::install::degems class to install necessary
-# Rubygems.
+# Installs OpenNebula from source
+# Creates and executes OpenNebula installation script based on given parameters.
+# This OpenNebula installation script is a wrapper around the 'install.sh'
+# script that comes with the OpenNebula source. It performs a self-contained
+# directory install in the specified directory (one_location). It will remove
+# all existing files in the one_location before performing the installation.
+#
+# The installation cab be either be 'all' core components install or only
+# 'sunstone' add-on install. The 'all' type install doesn't install 'sunstone'
+# component (not implemented in OpenNebula's install.sh script). And the
+# 'sunstone' type install doesn't install any of the core components.
+#
+# This 'installation' calls the 'degems' class for Rubygems installation before
+# starting with the OpenNebula installation. The degems class in turn calls
+# depackages class for system packages installation.
 #
 # == Parameters:
 #
-# * Parameter: Description (default)
-# * $one_install_script: OpenNebula installation script location (default: /tmp/one_install_script)
-# * $one_install_type: OpenNebula installation type - all, sunstone (default: default)
-# * $one_user: OpenNebula system user account (pavgi)
-# * $one_group: OpenNebula system group account (pavgi)
-# * $one_location: OpenNebula install location ($oneadmin_home/tmp-install/cloud/one)
-# * $one_db_backend: Database backend type - sqlite or mysql (Default: sqlite)
+# * Parameter: Description
+# * $one_install_script: OpenNebula installation script location
+# * $one_install_type: OpenNebula installation type - all, sunstone
+# * $one_user: OpenNebula system user account
+# * $one_group: OpenNebula system group account
+# * $one_location: OpenNebula install location
+# * $one_db_backend: Database backend type - sqlite or mysql
 #
-# == Supported Installation Types/Options:
-# This class creates and executes a wrapper script (around install.sh shipped with OpenNebula source)
-# for OpenNebula installation from source.
-# It will install OpenNebula in a self-contained directory. The installation type can either be
-# an 'all' core components install or only 'sunstone' add-on install. The 'all' type install doesn't
-# install 'sunstone' component (not implemented in OpenNebula's install.sh script). The 'sunstone' type
-# doesn't install any of the core components as well.
-# This class calls/requires Rubygems installation class (degems - dependency gems) before running the
-# installation script. The 'degems' class in turn ensures that required system packages are installed
-# for OpenNebula source and Rubygems.
-#
-# TODO/Issues: A Puppet class can't be defined or evaluated twice for the same node type. And hence right now
-# this implementation can't support both 'all' and 'sunstone' install for the same node. I am planning to add
-# Puppet define constructs soon to address this issue.
+# TODO/Issues:
+# A Puppet class can't be defined or evaluated twice for the same node type.
+# And hence right now this 'class' implementation can't support both 'all' and
+# 'sunstone' type install on the same node. I am planning to add Puppet define
+# constructs to address this issue.
 #
 # == Author(s):
 #   * Shantanu Pavgi, pavgi@uab.edu
 
 class uabopennebula::install::installation (
-  $one_install_script = "$uabopennebula::params::one_install_script",
-  $one_install_type = "$uabopennebula::params::one_install_type",
-  $one_db_backend = "$uabopennebula::params::one_db_backend",
-  $one_user = "$uabopennebula::params::one_user",
-  $one_group = "$uabopennebula::params::one_group",
-  $one_location = "$uabopennebula::params::one_location"
+  $one_install_script = $uabopennebula::params::one_install_script,
+  $one_install_type = $uabopennebula::params::one_install_type,
+  $one_db_backend = $uabopennebula::params::one_db_backend,
+  $one_user = $uabopennebula::params::one_user,
+  $one_group = $uabopennebula::params::one_group,
+  $one_location = $uabopennebula::params::one_location,
+  $one_install_gems_sunstone = $uabopennebula::params::one_install_gems_sunstone,
+  $one_install_gems_all = $uabopennebula::params::one_install_gems_all,
+  $one_install_gem_sqlite = $uabopennebula::params::one_install_gem_sqlite,
+  $one_install_gem_sqlite_version = $uabopennebula::params::one_install_gem_sqlite_version,
+  $one_install_packages_sunstone = $uabopennebula::params::one_install_packages_sunstone,
+  $one_install_packages_all = $uabopennebula::params::one_install_packages_all
 ) inherits uabopennebula::params {
 
+  # Rubygems should be in place before OpenNebula installation
+  # Call/Require/Evaluate degems class before evaluating installation class
   class{"uabopennebula::install::degems":
-    one_install_type => $one_install_type,
+    one_install_type               => $one_install_type,
+    one_install_gems_sunstone      => $one_install_gems_sunstone,
+    one_install_gems_all           => $one_install_gems_all,
+    one_install_gem_sqlite         => $one_install_gem_sqlite,
+    one_install_gem_sqlite_version => $one_install_gem_sqlite_version,
+    one_install_packages_sunstone  => $one_install_packages_sunstone,
+    one_install_packages_all       => $one_install_packages_all,
   }
-
   Class['uabopennebula::install::degems'] -> Class['uabopennebula::install::installation']
 
+  # Create install script based on one_user, one_location etc.
   file{$one_install_script:
     mode    => '0700',
     content => template('uabopennebula/one_install_script.erb'),
   }
 
+  # Install script purges all files in one_location before installing OpenNebula files
   exec{$one_install_script:
-    unless  => "/usr/bin/test -d ${one_location}",
     require => File[$one_install_script],
   }
 }
