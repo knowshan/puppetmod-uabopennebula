2dc4a266d208d49c59a8654c3e03ab1fcb2e7bd6
Shantanu Pavgi <pavgi@uab.edu>
2012-04-20 15:19:06 -0500

    gh-13  * Setup RSpec test environment by adding necessary fixtures, spec_helper and Rakefile  * Added couple of simple tests for onedconf class

---
+++ spec/classes/onedconf_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+# A very basic test to get started with rspec
+describe 'uabopennebula::conf::onedconf', :type => :class do
+
+  # Test with default parameters
+  context 'with default parameter values' do
+    let (:facts) {{:operatingsystem => 'CentOS',:rubyversion => '1.8.7'}}
+    it do
+      # onedconf class should contain a file with following params
+      # name/path: '/home/pavgi/tmp-install/cloud/one/etc/oned.conf'
+      # mode: 0640
+      # content: matched using RegEx as the Puppet manifest uses template function
+      should contain_file('/home/pavgi/tmp-install/cloud/one/etc/oned.conf').with({
+        'mode'     => '0640',
+        'content'  => /OpenNebula Configuration file/,
+      })
+      # should contain_file('/home/pavgi/tmp-install/cloud/one/etc/oned.conf').with_content(/OpenNebula Configuration file.*DB = [backend = sqlite3]/)
+    end
+  end
+
+  # Test by changing one_oned_conf_path
+  context 'with one_oned_conf_path => /srv/cloud/one/etc/oned.conf' do
+    let (:facts) {{:operatingsystem => 'CentOS',:rubyversion => '1.8.7'}}
+    let (:params) {{:one_oned_conf_path => '/srv/cloud/one/etc/oned.conf' }}
+    it do
+      should contain_file('/srv/cloud/one/etc/oned.conf').with({
+        'mode'     => '0640',
+        'content'  => /OpenNebula Configuration file/,
+      })
+    end
+  end
+end
